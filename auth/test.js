c54a23598d0a9353391175918aaf60544eb482df
Wagner Sodré <wagnersodre@MacBook-Pro-de-Wagner.local>
2021-05-03 16:05:02 -0300

    created login test

---
+++ auth/test.js
@@ -0,0 +1,63 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const dotenv = require('dotenv');
+
+const auth = require('./auth');
+const User = require('../models/user');
+
+dotenv.config();
+
+describe('Login', function() {
+  describe('When a invalid user tries to login', function() {
+    const sandbox = sinon.createSandbox();
+    it('should return an error', function() {
+      sandbox.stub(User, 'findOne').rejects(new Error());
+      let req = {
+        'body': {
+          'user': 'test',
+          'pwd': 'wrong'
+        }
+      };
+      let res = {
+        send: function(){ },
+        json: function(err){
+          expect(err.message).to.equal('Unauthorized');
+        },
+        status: function(responseStatus) {
+          expect(responseStatus).to.equal(401);
+          return this; 
+        }
+      };
+      auth.login(req, res);
+      // Restore sandbox.
+      sandbox.restore();
+    });
+  });
+
+  describe('When a valid user tries to login', function() {
+    const sandbox = sinon.createSandbox();
+    it('should return a token', function() {
+      const mockUser = { _id: '5e684ebacb19f70020661f44', name: 'test', password: 'test' };
+      sandbox.stub(User, 'findOne').returns(Promise.resolve(mockUser));
+      let req = {
+        'body': {
+          'user': 'test',
+          'pwd': 'test'
+        }
+      };
+      let res = {
+        send: function(){ },
+        json: function(res){
+          expect(res.auth).to.equal(true);
+        },
+        status: function(responseStatus) {
+          expect(responseStatus).to.equal(200);
+          return this; 
+        }
+      };
+      auth.login(req, res);
+      // Restore sandbox.
+      sandbox.restore();
+    });
+  });
+});
